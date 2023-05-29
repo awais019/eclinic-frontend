8257ada5a4021686e3afccbdfc3b5530364627e5
Muhammad Awais <awaistopper20@gmail.com>
2023-05-29 11:25:57 +0500

    Added desktop design for signup options.

--- components/SignUpOptions.vue
+++ components/SignUpOptions.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
+  <div class="hidden tablet:block">
+    <img src="~/assets/images/options.svg" alt="Decorative image" />
+  </div>
   <div class="signup-options">
     <h2 class="text-h2-sb">Create Free Account</h2>
     <p class="text-h5-reg">Choose an option below to continue</p>
@@ -27,14 +30,20 @@
 </template>
 
 <style lang="postcss" scoped>
+  img {
+    @apply mx-auto;
+  }
   .signup-options {
-    @apply mt-36;
+    @apply mobile:mt-36 tablet:max-w-[40%] mx-auto;
   }
   .options {
     @apply mt-8;
   }
   .options-list {
-    @apply flex flex-col gap-4 mt-4;
+    @apply flex flex-col gap-4 mt-4 tablet:flex-row;
+  }
+  .options-list a {
+    @apply w-full;
   }
   .option {
     @apply border-[1px] border-neutral-gallery rounded-lg w-full py-3
