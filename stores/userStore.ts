2c8dfa007683a8f8559dafc99753e31c43052d98
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-07-23 22:18:41 +0500

    Refactored patient composable.

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -2,12 +2,9 @@ import { Doctor, Patient } from "~/types/users";
 
 export default defineStore("userStore", () => {
   const user = ref<Doctor | Patient | null>(null);
-  const { registerDoctor } = useDoctor();
 
-  async function registerPatient(newPatient: Patient) {
-    const { data, error } = await useRegisterPatient(newPatient);
-    return { data, error };
-  }
+  const { registerPatient } = usePatient();
+  const { registerDoctor } = useDoctor();
 
   return { user, registerPatient, registerDoctor };
 });
