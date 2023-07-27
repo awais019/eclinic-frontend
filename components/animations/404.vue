179e066115b93450da45f7c4ac1c5f58a7c8b891
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-07-27 13:05:45 +0500

    Added error page.

---
+++ components/animations/404.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import animation from "~/assets/animations/404.json";
+  import { Vue3Lottie } from "vue3-lottie";
+
+  defineProps<{
+    height: number;
+    width: number;
+    loop: boolean;
+    autoPlay: boolean;
+  }>();
+</script>
+
+<template>
+  <Vue3Lottie
+    :animationData="animation"
+    :height="height"
+    :width="width"
+    :loop="loop"
+    :autoPlay="autoPlay"
+  ></Vue3Lottie>
+</template>
