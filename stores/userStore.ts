9b5603ac0d092f01987b53b386ed49d79edf7638
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-07-29 09:56:41 +0500

    Refactored store to move setup function to a seperate function.

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -1,42 +1,40 @@
 import { Doctor, Patient } from "~/types/users";
 
-export default defineStore(
-  "userStore",
-  () => {
-    const user = ref<Doctor | Patient | null>(null);
-    const authToken = ref<string | null>(null);
+export default defineStore("userStore", store, { persist: true });
 
-    const { registerPatient } = usePatient();
-    const { registerDoctor } = useDoctor();
-    const { signin, forgotpassword, resetpassword, me } = useAuth();
+function store() {
+  const user = ref<Doctor | Patient | null>(null);
+  const authToken = ref<string | null>(null);
 
-    async function userSignin(email: string, password: string) {
-      const { data, error } = await signin(email, password);
-      if (data.value) {
-        authToken.value = data.value.data.token;
-      }
-      await userMe();
-      return { data, error };
+  const { registerPatient } = usePatient();
+  const { registerDoctor } = useDoctor();
+  const { signin, forgotpassword, resetpassword, me } = useAuth();
+
+  async function userSignin(email: string, password: string) {
+    const { data, error } = await signin(email, password);
+    if (data.value) {
+      authToken.value = data.value.data.token;
     }
+    await userMe();
+    return { data, error };
+  }
 
-    async function userMe() {
-      const { data, error } = await me();
-      if (data.value) {
-        user.value = data.value.data;
-      }
-      return { data, error };
+  async function userMe() {
+    const { data, error } = await me();
+    if (data.value) {
+      user.value = data.value.data;
     }
+    return { data, error };
+  }
 
-    return {
-      user,
-      authToken,
-      registerPatient,
-      registerDoctor,
-      userSignin,
-      forgotpassword,
-      resetpassword,
-      userMe,
-    };
-  },
-  { persist: true }
-);
+  return {
+    user,
+    authToken,
+    registerPatient,
+    registerDoctor,
+    userSignin,
+    forgotpassword,
+    resetpassword,
+    userMe,
+  };
+}
