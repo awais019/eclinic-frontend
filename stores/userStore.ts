00f6d1d2d45559418ad4685b0aaa8afad225c6c2
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-07-29 09:54:58 +0500

    Added persistance to pinia store.

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -1,40 +1,42 @@
 import { Doctor, Patient } from "~/types/users";
 
-export default defineStore("userStore", () => {
-  const user = ref<Doctor | Patient | null>(null);
-  const authToken = ref<string | null>(null);
+export default defineStore(
+  "userStore",
+  () => {
+    const user = ref<Doctor | Patient | null>(null);
+    const authToken = ref<string | null>(null);
 
-  const { registerPatient } = usePatient();
-  const { registerDoctor } = useDoctor();
-  const { signin, forgotpassword, resetpassword, me } = useAuth();
+    const { registerPatient } = usePatient();
+    const { registerDoctor } = useDoctor();
+    const { signin, forgotpassword, resetpassword, me } = useAuth();
 
-  async function userSignin(email: string, password: string) {
-    const { data, error } = await signin(email, password);
-    if (data.value) {
-      authToken.value = data.value.data.token;
+    async function userSignin(email: string, password: string) {
+      const { data, error } = await signin(email, password);
+      if (data.value) {
+        authToken.value = data.value.data.token;
+      }
+      await userMe();
+      return { data, error };
     }
-    await userMe();
-    return { data, error };
-  }
 
-  async function userMe() {
-    const { data, error } = await me();
-    if (data.value) {
-      user.value = data.value.data;
+    async function userMe() {
+      const { data, error } = await me();
+      if (data.value) {
+        user.value = data.value.data;
+      }
+      return { data, error };
     }
-    console.log(user.value);
 
-    return { data, error };
-  }
-
-  return {
-    user,
-    authToken,
-    registerPatient,
-    registerDoctor,
-    userSignin,
-    forgotpassword,
-    resetpassword,
-    userMe,
-  };
-});
+    return {
+      user,
+      authToken,
+      registerPatient,
+      registerDoctor,
+      userSignin,
+      forgotpassword,
+      resetpassword,
+      userMe,
+    };
+  },
+  { persist: true }
+);
