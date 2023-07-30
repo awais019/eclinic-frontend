ec18be303b9dd668a7f9f4cfcca35d6ab4db4d7f
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-07-30 13:34:59 +0500

    Refactored components for dashboards.

---
+++ components/Dashboard.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div><PatientQuickActions /></div>
+</template>
+
+<style scoped></style>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -3,6 +3,7 @@
 <template>
   <div class="app-container">
     <DashboardProfileSection />
+    <Dashboard />
   </div>
 </template>
 
