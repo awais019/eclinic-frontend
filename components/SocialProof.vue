0e9d31f0f007abff19bcd2fd1def8c2b6af405c8
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-07-31 22:04:16 +0500

    Added design for social proof

---
+++ components/SocialProof.vue
@@ -0,0 +1,59 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="app-container text-center relative">
+    <img
+      src="~/assets/images/spiral.svg"
+      alt="spiral"
+      class="absolute -z-10 -top-10"
+    />
+    <div class="bg-white">
+      <h2 class="text-h2-b">What make us unique?</h2>
+      <p class="text-neutral-dusty-gray mb-12">
+        Discover a patient-centered approach to advanced medical care for your
+        appointment.
+      </p>
+      <div
+        class="text-neutral-dusty-gray text-sm grid grid-cols-2 gap-y-7 tablet:grid-cols-4"
+      >
+        <div
+          class="border-r-[1px] border-r-neutral-dusty-gray border-opacity-50 px-2"
+        >
+          <p class="text-primary-blue-ribbon text-h4-b">
+            255 <span class="text-neutral-mine-shaft">+</span>
+          </p>
+          <span>Expert doctors available</span>
+        </div>
+        <div
+          class="px-2 tablet:border-r-[1px] tablet:border-r-neutral-dusty-gray tablet:border-opacity-50"
+        >
+          <p class="text-primary-blue-ribbon text-h3-b">
+            100<span class="text-neutral-mine-shaft">%</span>
+          </p>
+          <span>Secure meeting booking</span>
+        </div>
+        <div
+          class="border-r-[1px] border-r-neutral-dusty-gray border-opacity-50 px-2"
+        >
+          <p class="text-primary-blue-ribbon text-h3-b">
+            99<span class="text-neutral-mine-shaft">%</span>
+          </p>
+          <span>Patient satisfaction rate</span>
+        </div>
+        <div class="px-2">
+          <p class="text-primary-blue-ribbon text-h3-b">
+            120<span class="text-neutral-mine-shaft">+</span>
+          </p>
+          <span>E-Clinic app downloads</span>
+        </div>
+      </div>
+    </div>
+    <img
+      src="~/assets/images/spiral.svg"
+      alt="spiral"
+      class="absolute -z-10 -bottom-10 right-0 transform rotate-180"
+    />
+  </div>
+</template>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@
 <template>
   <div>
     <HeroSection />
+    <SocialProof />
   </div>
 </template>
 
