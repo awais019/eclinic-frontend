9a629c129102716fa5fcbc0aaf184e491a732af1
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-08-06 21:43:35 +0500

    Fixed styles for quick actions

--- components/PatientQuickActions.vue
+++ components/PatientQuickActions.vue
@@ -7,7 +7,7 @@
       >Follow these easy steps for seamless appointments, physical or
       virtual</span
     >
-    <div class="mt-12 grid gap-8 tablet:grid-cols-2 desktop:grid-cols-3">
+    <div class="mt-12 flex flex-wrap gap-8 justify-center">
       <nuxt-link to="/doctors" class="group">
         <div
           class="px-10 py-12 shadow-variant4 rounded-2xl group-hover:bg-primary-blue-ribbon group-hover:text-white"
@@ -18,7 +18,8 @@
             <IconsPeople />
           </div>
           <h3 class="text-h4-b mt-12 mb-3">Find Doctors</h3>
-          <span class="text-neutral-dusty-gray group-hover:text-white"
+          <span
+            class="text-neutral-dusty-gray group-hover:text-white max-w-[40ch] inline-block"
             >Find highly qualified doctors and get an appointment.</span
           >
         </div>
@@ -33,7 +34,8 @@
             <IconsCalendar />
           </div>
           <h3 class="text-h4-b mt-12 mb-3">Appointments</h3>
-          <span class="text-neutral-dusty-gray group-hover:text-white"
+          <span
+            class="text-neutral-dusty-gray max-w-[40ch] inline-block group-hover:text-white"
             >Check your upcoming appoinments and all actions related to
             appoinments.</span
           >
@@ -49,7 +51,8 @@
             <IconsReport />
           </div>
           <h3 class="text-h4-b mt-12 mb-3">Reports</h3>
-          <span class="text-neutral-dusty-gray group-hover:text-white"
+          <span
+            class="text-neutral-dusty-gray group-hover:text-white max-w-[40ch] inline-block"
             >Stay up-to-date with your health by accessing your reports
             online.</span
           >
@@ -65,7 +68,8 @@
             <IconsPrescription />
           </div>
           <h3 class="text-h4-b mt-12 mb-3">Access Prescription</h3>
-          <span class="text-neutral-dusty-gray group-hover:text-white"
+          <span
+            class="text-neutral-dusty-gray group-hover:text-white max-w-[40ch] inline-block"
             >View, download, or print your prescription easily through
             platform.</span
           >
@@ -81,7 +85,8 @@
             <IconsCard />
           </div>
           <h3 class="text-h4-b mt-12 mb-3">Payments</h3>
-          <span class="text-neutral-dusty-gray group-hover:text-white"
+          <span
+            class="text-neutral-dusty-gray group-hover:text-white max-w-[40ch] inline-block"
             >Skip the hassle of mailing checks or paying in person.</span
           >
         </div>
@@ -96,7 +101,8 @@
             <IconsFlask />
           </div>
           <h3 class="text-h4-b mt-12 mb-3">Find Labs</h3>
-          <span class="text-neutral-dusty-gray group-hover:text-white"
+          <span
+            class="text-neutral-dusty-gray group-hover:text-white max-w-[40ch] inline-block"
             >Find labs that can get samples from your home and test.</span
           >
         </div>
