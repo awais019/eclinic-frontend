151b95c6034d0f26c4d91c5bbf3aff1908bf8bd3
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-08-06 21:48:27 +0500

    Fixed design for categories

--- components/Categories.vue
+++ components/Categories.vue
@@ -8,7 +8,7 @@
         Simple Steps For Your Solution
       </h3>
       <p>Follow these easy steps for seamless booking, offline or online</p>
-      <div class="mt-12 grid gap-8 tablet:grid-cols-2 desktop:grid-cols-3">
+      <div class="mt-12 flex flex-wrap gap-8 items-center justify-center">
         <div
           class="px-10 py-12 shadow-variant4 rounded-2xl group-hover:bg-primary-blue-ribbon group-hover:text-white"
         >
@@ -20,7 +20,7 @@
           <h3 class="text-h4-b mt-12 mb-3 text-neutral-mine-shaft">
             Find Doctors
           </h3>
-          <span class="text-neutral-dusty-gray"
+          <span class="text-neutral-dusty-gray max-w-[40ch] inline-block"
             >Find highly qualified doctors and get an appointment.</span
           >
         </div>
@@ -35,7 +35,7 @@
           <h3 class="text-h4-b mt-12 mb-3 text-neutral-mine-shaft">
             Make Appointment
           </h3>
-          <span class="text-neutral-dusty-gray"
+          <span class="text-neutral-dusty-gray max-w-[40ch] inline-block"
             >Make doctor appointment online or by phone with ease.</span
           >
         </div>
@@ -48,7 +48,7 @@
             <IconsReport />
           </div>
           <h3 class="text-h4-b mt-12 mb-3 text-neutral-mine-shaft">Reports</h3>
-          <span class="text-neutral-dusty-gray"
+          <span class="text-neutral-dusty-gray max-w-[40ch] inline-block"
             >Stay up-to-date with your health by accessing your reports
             online.</span
           >
@@ -64,7 +64,7 @@
           <h3 class="text-h4-b mt-12 mb-3 text-neutral-mine-shaft">
             Access Prescription
           </h3>
-          <span class="text-neutral-dusty-gray"
+          <span class="text-neutral-dusty-gray max-w-[40ch] inline-block"
             >View, download, or print your prescription easily through
             platform.</span
           >
@@ -80,7 +80,7 @@
           <h3 class="text-h4-b mt-12 mb-3 text-neutral-mine-shaft">
             Pay Online
           </h3>
-          <span class="text-neutral-dusty-gray"
+          <span class="text-neutral-dusty-gray max-w-[40ch] inline-block"
             >Skip the hassle of mailing checks or paying in person.</span
           >
         </div>
@@ -93,7 +93,7 @@
             <IconsFlask />
           </div>
           <h3 class="text-h4-b mt-12 mb-3 text-neutral-mine-shaft">Labs</h3>
-          <span class="text-neutral-dusty-gray"
+          <span class="text-neutral-dusty-gray max-w-[40ch] inline-block"
             >Find Labs to get your samples from your home and get your tests
             done.</span
           >
