515c1ecb08b71e504d86b629065ec4bfdc167015
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-08-06 19:41:44 +0500

    Added composable to fetch doctors

--- composables/useDoctor.ts
+++ composables/useDoctor.ts
@@ -1,13 +1,23 @@
 export function useDoctor() {
+  const { baseURL } = useRuntimeConfig().public;
   async function registerDoctor(newDoctor: FormData) {
     const { data, error } = await useFetch("/doctors/register", {
       method: "POST",
       body: newDoctor,
-      baseURL: useRuntimeConfig().public.baseURL,
+      baseURL,
     });
 
     return { data, error };
   }
 
-  return { registerDoctor };
+  async function getDoctors() {
+    const { data, error } = await useFetch("/doctors", {
+      method: "GET",
+      baseURL,
+    });
+
+    return { data, error };
+  }
+
+  return { registerDoctor, getDoctors };
 }
--- pages/doctors.vue
+++ pages/doctors.vue
@@ -3,11 +3,23 @@
     { name: "Home", path: "/" },
     { name: "Find doctors", path: "/doctors" },
   ]);
+
+  const { getDoctors } = useDoctor();
+
+  const doctors = await getDoctors();
 </script>
 
 <template>
   <PageTitle title="Find doctors" :bread-crumbs="breadCrumbs" />
-  <div class="app-container"></div>
+  <div class="app-container mb-12">
+    <div class="text-center">
+      <p class="text-base-md text-primary-blue-ribbon">Meet our Doctors</p>
+      <h2 class="text-h2-b">Our Highly Qualified Professionals</h2>
+    </div>
+    <pre>
+      {{ doctors }}
+    </pre>
+  </div>
 </template>
 
 <style scoped></style>
