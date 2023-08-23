e60202b9eb7a68356e3584c112ea4135414b8cf0
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-08-23 16:01:43 +0500

    Added index in profile setup

---
+++ components/profilesetup/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  import { storeToRefs } from "pinia";
+  import useUserStore from "~/stores/userStore";
+
+  defineEmits<{
+    (e: "continue"): void;
+  }>();
+
+  const userStore = useUserStore();
+
+  const { first_name, last_name } = storeToRefs(userStore);
+</script>
+
+<template>
+  <div>
+    <p class="text-h5">
+      Hi, {{ first_name }} {{ last_name }}! you are required to setup some extra
+      information in your profile to use our app. Thank you for your
+      cooperation!
+    </p>
+    <button
+      class="float-right bg-primary-blue-ribbon text-white font-semibold py-3 px-4 rounded-lg"
+      @click="$emit('continue')"
+    >
+      Continue
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
--- pages/profilesetup.vue
+++ pages/profilesetup.vue
@@ -2,10 +2,31 @@
   definePageMeta({
     layout: false,
   });
+
+  function goToPhone() {
+    console.log("go to phone");
+  }
 </script>
 
 <template>
-  <div>Profile Setup</div>
+  <DecorativeLogo align="right" />
+  <div
+    class="tablet:grid tablet:grid-flow-col tablet:grid-cols-2 tablet:h-screen desktop:gap-40 tablet:gap-20 mobile:app-container"
+  >
+    <div
+      class="hidden bg-primary-blue-ribbon tablet:flex tablet:items-center tablet:justify-center"
+    >
+      <img src="~/assets/images/signin.svg" alt="Decorative image" />
+    </div>
+    <div
+      class="flex flex-col gap-4 justify-center mt-12 tablet:mt-0 tablet:max-w-[30.5rem]"
+    >
+      <h1 class="text-h2-sb text-center text-primary-blue-ribbon">
+        Profile Setup
+      </h1>
+      <Profilesetup @continue="goToPhone" />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
