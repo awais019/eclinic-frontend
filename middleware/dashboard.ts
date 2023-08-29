fc5bfc44461b5d0175ec806e03608056c67face3
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-08-29 15:32:28 +0500

    Fixed redirect path

--- middleware/dashboard.ts
+++ middleware/dashboard.ts
@@ -3,7 +3,7 @@ import useUserStore from "~/stores/userStore";
 export default defineNuxtRouteMiddleware((to, from) => {
   const userStore = useUserStore();
   if (!userStore.isLoggedIn) {
-    return `/signin?redirect=/${to.path}`;
+    return `/signin?redirect=${to.path}`;
   } else if (userStore.isPatient && to.path !== "/dashboard/patient") {
     return "/dashboard/patient";
   } else if (
