7da25818d17949f043e910852931ca753155e16d
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-09-03 14:30:31 +0500

    Added action back for commpleted appointment

--- components/doctor/CompletedAppointements.vue
+++ components/doctor/CompletedAppointements.vue
@@ -23,6 +23,7 @@
             <th class="py-6 px-2 whitespace-nowrap">Date & Time</th>
             <th class="py-6 px-2 whitespace-nowrap">Type</th>
             <th class="py-6 px-2 whitespace-nowrap">Amount</th>
+            <th class="py-6 pr-6 pl-2 whitespace-nowrap">Action</th>
           </tr>
         </thead>
         <div class="absolute h-[1px] left-6 right-6 bg-neutral-gallery"></div>
@@ -48,6 +49,13 @@
             </td>
             <td class="py-6 px-2 text-neutral-dusty-gray whitespace-nowrap">
               {{ appointment.charges }}
+            </td>
+            <td class="py-6 pr-6 pl-2 flex whitespace-nowrap">
+              <button
+                class="bg-primary-blue-ribbon px-4 py-2 rounded-full flex gap-1 items-center text-white font-semibold text-sm"
+              >
+                Prescribe
+              </button>
             </td>
           </tr>
         </tbody>
